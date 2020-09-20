38d52bee6e6b408ce1842da6f022a9b671c14d44
tollii <tolnes@protonmail.com>
2020-09-20 21:42:30 +0200

    Finished assignment 8

---
+++ assignment_8/2/client/src/App.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './App.css';
+const pbkdf2 = require('pbkdf2');
+
+function App() {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    const logIn = () => {
+        axios.post('http://localhost:5000/auth',
+            {
+                username: username,
+                password: password
+            })
+            .then(res => {
+                localStorage.setItem('bearer', res.headers.authorization);
+
+                const header = {
+                    headers: {
+                        Authorization: localStorage.getItem('bearer'),
+                        'Content-Type': 'application/json;charset=UTF-8',
+                        'Access-Control-Allow-Origin': '*'
+                    }
+                };
+
+                axios.get('http://localhost:5000/hello', header)
+                    .then(res => {
+                        if (res.status === 200) {
+                            console.log('Auth OK');
+                            setLoggedIn(true);
+                        } else {
+                            setLoggedIn(false);
+                        }
+
+                    });
+            });
+    };
+
+    const handleUsername = event => {
+        setUsername(event.target.value);
+    };
+
+    const handlePassword = event => {
+        const salt = 'salt';
+        const hash = pbkdf2.pbkdf2Sync(password, salt, 2048, 15, 'sha512').toString('hex');
+        setPassword(hash);
+    };
+
+
+    const handleLoginButton = () => {
+        console.log('Logging in...');
+        logIn()
+
+
+    };
+
+    const handleRegisterButton = () => {
+        console.log('Registering...');
+        axios.post('http://localhost:5000/register',
+            {
+                username: username,
+                password: password
+            })
+            .then(res => {
+                (res.status === 200) ? alert(`You've been registered!`) : alert('Something went wrong :/');
+            })
+            .catch(err => console.log(err));
+
+    };
+
+
+  return (
+    <div className='App'>
+        <div>
+            <input onChange={handleUsername} placeholder='Username'/>
+            <input onChange={handlePassword} type={'password'} placeholder='Password'/>
+        </div>
+
+        <div>
+            <button onClick={handleRegisterButton} type='button'>Register</button>
+            <button onClick={handleLoginButton} type='button'>Login</button>
+        </div>
+
+        {loggedIn ? <h1>You're authorized!</h1> : ''}
+
+    </div>
+
+  );
+}
+
+
+export default App;
